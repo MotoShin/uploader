bb522e40f55c24cd2473d5bc4bf5624aca4a92b6
Moto Shinriki <m8o8t2o8@gmail.com>
2017-04-20 10:44:53 +0900

    -a

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'fileuploads/index'
+  #
+  # get 'fileuploads/create'
+  #
+  # get 'fileuploads/new'
+
+  resources :fileuploads, only: [:index, :create, :new]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
